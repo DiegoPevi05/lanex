8f0a771f7a3b0a70bfddd51cc96003705d44f122
diegopevi05 <diego10azul@hotmail.com>
2024-11-26 13:19:25 -0500

    version web good

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="@yield('meta_description', __('messages.meta.home'))">
 
-        <title>Lanex</title>
+        <title>LanEx</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
